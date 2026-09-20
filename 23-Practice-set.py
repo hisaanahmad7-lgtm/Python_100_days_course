5b74eea2b7004ca90724d3a8976421f61035cb92
Mr.Hackman <hisaanahmad7@gmail.com>
2026-09-20 13:26:46 -0400

    23-Practice-set.py
    This file is changed by Hisaan Ahamd

--- 23-Practice-set.py
+++ 23-Practice-set.py
@@ -1,18 +1,18 @@
-# q1 = input("Enter your name : ")
-# q2 = "Good Morning"
-# print(f"{q2} How are you {q1}" , q1.title())
+q1 = input("Enter your name : ")
+q2 = "Good Morning"
+print(f"{q2} How are you {q1}" , q1.title())
 
-# a1 = input("Enter your name : ")
-# a1.title()
-# w1 = "22-August-2025"
-# print(f"Dear {a1} \n You are selected @ {w1} As An AI Engennier! " )
+a1 = input("Enter your name : ")
+a1.title()
+w1 = "22-August-2025"
+print(f"Dear {a1} \n You are selected @ {w1} As An AI Engennier! " )
 
-# text = "Hello  Hisaan"
+text = "Hello  Hisaan"
 
-# if " " in text:
-#     print("yes")
-# else:
-#     print("no")
+if " " in text:
+    print("yes")
+else:
+    print("no")
 
 
 
