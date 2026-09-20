b47bfd010a9289f410fffdab9fd2854532652893
hisaanahmad7-lgtm <hisaanahmad7@gmail.com>
2026-09-20 12:46:04 -0400

    15-ListMethods.py
    This file is edited by Hisaan Ahmad

--- 15-ListMethods.py
+++ 15-ListMethods.py
@@ -1,38 +1,38 @@
-# l1 = [1, 2, 3 , 12,44,68,90,20] 
-# print(l1)
-# l1.append(122)
-# print(l1)
-# l1.pop(2)
-# print(l1)
-# l1.sort()
-# print(l1)
-# l1.sort(reverse=True)
-# print("The reverse of list is = " , l1)
+l1 = [1, 2, 3 , 12,44,68,90,20] 
+print(l1)
+l1.append(122)
+print(l1)
+l1.pop(2)
+print(l1)
+l1.sort()
+print(l1)
+l1.sort(reverse=True)
+print("The reverse of list is = " , l1)
 
-# q1 = ["Banana" , "Mango" , "Cherry" , "Pineapple" , "Peach" , "Peach"]
-# q1.extend("Name")
-# print(q1)
-
-
-# vowels = ["a", "e", "o", "u"]
-# vowels.insert(2, "i")  
-# print(vowels)  
-
-# vowels.remove("a")
-# print(vowels)
-
-# data = [1, 2, 3, 4]
-# data.clear()
-# print(data)  
-
-# abc = ["a", "b", "c"]
-# abc.reverse()
-# print(abc)  # Output: ['c', 'b', 'a']
+q1 = ["Banana" , "Mango" , "Cherry" , "Pineapple" , "Peach" , "Peach"]
+q1.extend("Name")
+print(q1)
 
 
-# letters = ["H", "i", "s", "a", "a", "n"]
-# name = "".join(letters)
-# print(name)  # Output: Hisaan
+vowels = ["a", "e", "o", "u"]
+vowels.insert(2, "i")  
+print(vowels)  
+
+vowels.remove("a")
+print(vowels)
+
+data = [1, 2, 3, 4]
+data.clear()
+print(data)  
+
+abc = ["a", "b", "c"]
+abc.reverse()
+print(abc)  # Output: ['c', 'b', 'a']
+
+
+letters = ["H", "i", "s", "a", "a", "n"]
+name = "".join(letters)
+print(name)  # Output: Hisaan
 
 
 myList = [1,2,2,2,3,4,4,5,5,6,7,8,9]
